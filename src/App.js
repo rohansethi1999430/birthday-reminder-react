c3e78022810a74a5080860ccf2d44b0f530add41
Rohan <rohansethi1999430@gmail.com>
2021-06-16 23:18:29 +0530

    Created basic container structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <main>
+    <section className='container'>
+      <h3>
+        0 birthdays today
+      </h3>
+      <List/>
+      <button onClick={()=>{console.log('you clicked me!!!')}}>
+        Clear all
+      </button>
+    </section>
+  </main>;
 }
 
 export default App;
